3119894b8f40cc9fb87cde66c5737d1698747e6d
Diwakar Mishra <diwakar.mishra01@nagarro.com>
2026-09-19 17:36:19 +0530

    Fix routing for GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import {
+  provideRouter,
+  withHashLocation
+} from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
+
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+
+    provideRouter(
+      routes,
+      withHashLocation()
+    )
+
   ]
+
 };
